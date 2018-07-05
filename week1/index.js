783ad61b1e2db67296f43b6781ea6861dd8a643f
Bella Chen <hchen045@seattlecentral.edu>
2018-07-05 23:50:10 +0000

    First commit

--- week1/index.js
+++ week1/index.js
@@ -1,18 +1,32 @@
-var http = require('http'); //gives app access to HTTP module
-var url = require('url'); //the url module splits the web address into readable parts
-var fs = require('fs'); //allows you to work with file system on your computer
-
-http.createServer(function (req, res) { //access to HTTP module, enables ability to create a server object
-                                        //req = request from the client as an object (http.IncomingMessage object)
-  var q = url.parse(req.url, true);  //req has property called "url" which holds the part of the url that comes after domain name
-  var filename = "./" + q.pathname + '.html'; //in subfolder
-  fs.readFile(filename, function(err, data) { //method used to read the files
-    if (err) { //if no filename exists
-      res.writeHead(404, {'Content-Type': 'text/html'}); //specify correct content type if html is to display
-      return res.end("404 Not Found"); //return error message
-    }  
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data); //display html content on page
-    return res.end(); //end of response
-  });
+var http = require("http"); 
+http.createServer(function(req,res) {
+  var path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      var fs = require("fs");
+      fs.readFile('static/home.html', function (err, data) {
+         if (err) {
+           return console.error(err);
+         } 
+         res.writeHead(200, {'Content-Type': 'text/html'});
+         res.write(data);
+         return res.end();
+      });
+      break;
+    case '/about':
+      var fs = require("fs");
+      fs.readFile('static/about.html', function (err, data) {
+         if (err) {
+           return console.error(err);
+         }
+         res.writeHead(200, {'Content-Type': 'text/html'});
+         res.write(data);
+         return res.end();
+      });
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
 }).listen(process.env.PORT || 3000);
